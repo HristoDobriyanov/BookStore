c130bc3ca3b1286812d2f6e98ce7e64cd409e1fa
Hristo Dobriyanov <hdobriyanov@gmail.com>
2019-06-04 14:56:29 +0300

    Models created
    Created new models for books, author and borrower.

---
+++ BookStore.Models/Book.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Reflection.Metadata;
+
+namespace BookStore.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string CoverImage { get; set; }
+
+        public int AuthorId { get; set; }
+
+        public Author Author { get; set; }
+
+        public ICollection<Borrower> Borrowers { get; set; }
+
+    }
+}
---
+++ BookStore.Models/Borrower.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace BookStore.Models
+{
+    public class Borrower
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public ICollection<Book> BorrowedBooks { get; set; }
+    }
+}
